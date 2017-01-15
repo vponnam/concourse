643819fe6b7d252987f6d92c590f089eb756821b
vponnam-pivotal <ponnam@Venkatas-MacBook-Pro.local>
2017-01-14 19:04:41 -0500

    script changes

--- workout/smoke-tests/pcf-smoke-test.sh
+++ workout/smoke-tests/pcf-smoke-test.sh
@@ -3,11 +3,11 @@
 #set -x
 
 apt-get update
-apt-get install git -y
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+apt-get install wget git -y
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 apt-get update
-apt-get install cf-cli
+apt-get install cf-cli -y
 
 #sample apps
 mkdir onetime-directory
