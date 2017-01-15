5394cf9d982da2f5b7e73f5c9b35e0853a818b43
vponnam-pivotal <ponnam@Venkatas-MacBook-Pro.local>
2017-01-15 18:45:24 -0500

    email script changes

--- workout/email/email.sh
+++ workout/email/email.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -xu
 
-ls -lR email-out/*
-ls -lR email-resource/*
+ls email-resource/*
 pwd
 SHA=$(shasum $file_to_hash | cut -d ' ' -f1)
 
@@ -11,6 +10,5 @@ output_subject_file=email-out/$output_subject_file
 
 echo -e "Email resource demo on $(date)" > $output_subject_file
 echo -e "SHA1 Hash of \"$file_to_hash\" is $SHA\n\nSucess!\n" > $output_body_file
-
-ls -lR email-out/*
-ls -lR email-resource/*
+echo "host is $host"
+ls email-out/*
