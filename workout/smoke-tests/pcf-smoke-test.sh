b604b91601c3cd5a5ae9bc7f65d6835f0602cdd0
vponnam <vponnam>
2017-05-08 09:44:18 -0400

    debug

--- workout/smoke-tests/pcf-smoke-test.sh
+++ workout/smoke-tests/pcf-smoke-test.sh
@@ -123,6 +123,7 @@ cf cs p-redis shared-vm $i5
 cf bs redis-example-app $i5
 cf start redis-example-app
 route=`cf app redis-example-app |grep "urls" | awk '{print $2}'`
+echo $route
 r1=`curl -X PUT $route/foo -d 'data=bar'`
 printf "\nInserting data to Redis Cache"
 r2=`curl -X GET $route/foo`
