0e280e39c9c4cca0c15ece7a8320e2a407dee00c
vponnam <vponnam>
2017-10-09 11:24:56 -0400

    health scripts will now output app name and it's realtime diego IP

--- workout/endpoint-test/load1.sh
+++ workout/endpoint-test/load1.sh
@@ -3,5 +3,6 @@
 while true
 do
         curl -k $r1 | tr ',' '\n' | grep "404 Not Found" | head -2 | tail -1
+        cf curl /v2/apps/$(cf app $(cf apps | grep $r1 |awk '{print $1}') --guid)/stats | grep -E "name|host"
         sleep 2
 done
--- workout/endpoint-test/load2.sh
+++ workout/endpoint-test/load2.sh
@@ -3,5 +3,6 @@
 while true
 do
         curl -k $r2 | tr ',' '\n' | grep "404 Not Found" | head -2 | tail -1
+        cf curl /v2/apps/$(cf app $(cf apps | grep $r2 |awk '{print $1}') --guid)/stats | grep -E "name|host"
         sleep 2
 done
