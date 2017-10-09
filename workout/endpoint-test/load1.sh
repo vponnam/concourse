846da044dd9cddbf53831cab5fbf12dfaf34fcf2
vponnam <vponnam>
2017-10-09 11:57:08 -0400

    debugging

--- workout/endpoint-test/load1.sh
+++ workout/endpoint-test/load1.sh
@@ -4,6 +4,7 @@ set -x
 while true
 do
         curl -k $r1 | tr ',' '\n' | grep "404 Not Found" | head -2 | tail -1
-        echo $(cf curl /v2/apps/$(cf app $(cf apps | grep $r1 |awk '{print $1}') --guid)/stats | grep -E "name|host")
+        cf apps && cf -v
+#        echo $(cf curl /v2/apps/$(cf app $(cf apps | grep $r1 |awk '{print $1}') --guid)/stats | grep -E "name|host")
         sleep 2
 done
