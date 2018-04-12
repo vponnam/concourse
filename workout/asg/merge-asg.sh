44f59ffb18fe04313f52d85605fd93d467bc2ef6
vponnam <vponnam>
2018-04-12 07:14:49 +0530

    Change in path

--- workout/asg/merge-asg.sh
+++ workout/asg/merge-asg.sh
@@ -3,3 +3,5 @@ set -e
 export TERM=xterm
 
 jq 'reduce inputs as $i (.; .resources += $i.resources)' src/workout/asg/asg-files/org-*.json > src/workout/asg/mergedasg.json
+
+cat src/workout/asg/mergedasg.json
