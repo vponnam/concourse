bad4b4f6afc454351d3ece40b70c7898b1b284dd
vponnam <vponnam>
2018-04-12 10:16:43 +0530

    Testing

--- workout/asg/apply-asg.sh
+++ workout/asg/apply-asg.sh
@@ -2,5 +2,12 @@
 set -e
 export TERM=xterm
 
+#merge individual json files
+jq 'reduce inputs as $i (.; .resources += $i.resources)' src/workout/asg/asg-files/org-*.json > src/workout/asg/mergedasg.json
+
 #execute the go-binary
+if [[$? = 0]];
+then
 ./src/workout/asg/asg
+else echo "Merging all the individual json files in asg-files folder failed"
+fi
